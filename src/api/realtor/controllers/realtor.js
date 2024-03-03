734b4b123b99dc6edeafc582b9167d7f36f7c7fa
Franco Monzon <monzondefranco@gmail.com>
2024-03-03 17:39:30 -0300

    [Realtors/Cities] Add Realtors and Cities

---
+++ src/api/realtor/controllers/realtor.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * realtor controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::realtor.realtor');
